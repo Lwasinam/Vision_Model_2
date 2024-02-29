403127dcfe3f7ef58e581ed572e0b180c1b1d466
Lwasinam <lwasinamdilli@gmail.com>
2024-02-29 15:20:21 +0100

    len

--- dataset.py
+++ dataset.py
@@ -28,91 +28,91 @@ class BilingualDataset(IterableDataset):
 
     # def __len__(self):
     #     return 1200000
-    def __getitem__(self):
-        pass
+    # def __getitem__(self):
+    #     pass
     def __iter__(self):
         return iter (self.generate())
     def generate(self):
-        
+        for item in self.ds:
 
         
-        src_image = self.ds['jpg']
-        tgt_text = self.ds['txt']
+            src_image = item['jpg']
+            tgt_text = item['txt']
 
-        # base64_bytes = base64.b64encode(src_image)
-       
-
-        # src_image = base64_bytes.decode()
+            # base64_bytes = base64.b64encode(src_image)
         
 
-        # src_image  = Image.open(BytesIO(b64decode(''.join(src_image))))
-
-        if src_image.mode != 'RGB':
-            src_image = src_image.convert('RGB')
-            
-        enc_input = feature_extractor(
-        src_image,
-        return_tensors='pt'
-        )
+            # src_image = base64_bytes.decode()
             
 
-            # Transform the text into tokens
-        dec_input_tokens = self.tokenizer_tgt.encode(tgt_text)
+            # src_image  = Image.open(BytesIO(b64decode(''.join(src_image))))
 
-            # # Add sos, eos and padding to each sentence
-            # enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2  # We will add <s> and </s>
-            # We will only add <s>, and </s> only on the label
-        dec_input_tokens = dec_input_tokens[:self.seq_len-1]
-        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) -1
-
-            # Make sure the number of padding tokens is not negative. If it is, the sentence is too long
-        if dec_num_padding_tokens < 0:
-            raise ValueError("Sentence is too long")
-
-            # # Add <s> and </s> token
-            # encoder_input = torch.cat(
-            #     [
-            #         self.sos_token,
-            #         torch.tensor(enc_input_tokens, dtype=torch.int64),
-            #         self.eos_token,
-            #         torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64),
-            #     ],
-            #     dim=0,
-            # )
-
-            # Add only <s> token
-        decoder_input = torch.cat(
-                [
-                    self.sos_token,
-                    torch.tensor(dec_input_tokens, dtype=torch.int64),
-                    torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64),
-                ],
-                dim=0,
+            if src_image.mode != 'RGB':
+                src_image = src_image.convert('RGB')
+                
+            enc_input = feature_extractor(
+            src_image,
+            return_tensors='pt'
             )
+                
 
-            # Add only </s> token
-        label = torch.cat(
-                [
-                    torch.tensor(dec_input_tokens, dtype=torch.int64),
-                    self.eos_token,
-                    torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64),
-                ],
-                dim=0,
-            )
+                # Transform the text into tokens
+            dec_input_tokens = self.tokenizer_tgt.encode(tgt_text)
+
+                # # Add sos, eos and padding to each sentence
+                # enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2  # We will add <s> and </s>
+                # We will only add <s>, and </s> only on the label
+            dec_input_tokens = dec_input_tokens[:self.seq_len-1]
+            dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) -1
+
+                # Make sure the number of padding tokens is not negative. If it is, the sentence is too long
+            if dec_num_padding_tokens < 0:
+                raise ValueError("Sentence is too long")
+
+                # # Add <s> and </s> token
+                # encoder_input = torch.cat(
+                #     [
+                #         self.sos_token,
+                #         torch.tensor(enc_input_tokens, dtype=torch.int64),
+                #         self.eos_token,
+                #         torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64),
+                #     ],
+                #     dim=0,
+                # )
+
+                # Add only <s> token
+            decoder_input = torch.cat(
+                    [
+                        self.sos_token,
+                        torch.tensor(dec_input_tokens, dtype=torch.int64),
+                        torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64),
+                    ],
+                    dim=0,
+                )
+
+                # Add only </s> token
+            label = torch.cat(
+                    [
+                        torch.tensor(dec_input_tokens, dtype=torch.int64),
+                        self.eos_token,
+                        torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64),
+                    ],
+                    dim=0,
+                )
 
 
-        assert decoder_input.size(0) == self.seq_len
-        assert label.size(0) == self.seq_len
+            assert decoder_input.size(0) == self.seq_len
+            assert label.size(0) == self.seq_len
 
-        yield {
-                "encoder_input": enc_input['pixel_values'][0],  # (seq_len)
-                "decoder_input": decoder_input,  # (seq_len)
-                "encoder_mask": (torch.cat((torch.ones(197,),torch.zeros(63),),)).unsqueeze(0).unsqueeze(0), # (1, 1, seq_len)
-                "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).int() & causal_mask(decoder_input.size(0)), # (1, seq_len) & (1, seq_len, seq_len),
-                "label": label,  # (seq_len)
-                # "src_text": src_text,
-                "tgt_text": tgt_text,
-            }
+            yield {
+                    "encoder_input": enc_input['pixel_values'][0],  # (seq_len)
+                    "decoder_input": decoder_input,  # (seq_len)
+                    "encoder_mask": (torch.cat((torch.ones(197,),torch.zeros(63),),)).unsqueeze(0).unsqueeze(0), # (1, 1, seq_len)
+                    "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).int() & causal_mask(decoder_input.size(0)), # (1, seq_len) & (1, seq_len, seq_len),
+                    "label": label,  # (seq_len)
+                    # "src_text": src_text,
+                    "tgt_text": tgt_text,
+                }
     
 def causal_mask(size):
     mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
