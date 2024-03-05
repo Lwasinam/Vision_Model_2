228b2e7d95366cb480e7d71cc5300f3480d2f63b
Lwasinam <lwasinamdilli@gmail.com>
2024-03-05 08:43:58 +0100

    mixed precision

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ def get_config():
         "num_epochs": 20,
         "lr": 1e-5,
         "seq_len": 261,
-        "d_model": 512,
+        "d_model": 1024,
         "lang_src": "en",
         "lang_tgt": "it",
         "model_folder": "weights",
