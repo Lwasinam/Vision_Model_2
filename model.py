c538151d8144ed3166eb9ff84977f6f0457ff517
Lwasinam <lwasinamdilli@gmail.com>
2024-03-06 15:00:09 +0100

    vit

--- model.py
+++ model.py
@@ -265,12 +265,12 @@ class EncoderBlock(nn.Module):
         self.self_attention_block = self_attention_block
         self.feed_forward_block = feed_forward_block
         self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
-        self.layer = layer
+        # self.layer = layer
 
     def forward(self, x, src_mask):
         # print(x.shape)
-        print(self.layer)
-        x = x[self.layer]
+        # print(self.layer)
+        x = x[layer]
         x = self.residual_connections[1](x, self.feed_forward_block)
         return x
     
